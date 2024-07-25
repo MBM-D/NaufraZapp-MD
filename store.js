a17d1916b1e1b1e42dbfafbb3d8739ee10c82d91
𖥔 ࣪˖🍁「𝗠𝘂𝗹𝘁𝗶𝗕𝗼𝘁┃ᵒᶠᶜ-ᵃᶜᵒᵘⁿᵗ」🍁𖥔 ࣪˖ <alanluliovargas@gmail.com>
2024-07-25 03:48:12 -0300

    • ⏣ 𝙉𝘢𝘶𝘧𝘳𝘢𝙕𝘢𝘱𝘱-𝙈𝘋 ⏣ •

--- store.js
+++ store.js
@@ -22,22 +22,22 @@ global.nz4 = fs.readFileSync('./storage/nz4.jpg')
 global.nz5 = fs.readFileSync('./storage/nz5.jpg')
 global.nz6 = fs.readFileSync('./storage/nz6.jpg')
 
-var img = 'https://i.postimg.cc/Y9t2mzvt/Menu2.jpg';
-var img1 = 'https://i.postimg.cc/T386DJsB/Menu3.jpg';
-var img2 = 'https://i.postimg.cc/T386DJsB/Menu3.jpg';
-var img3 = 'https://i.postimg.cc/43sR8Ns5/Menu4.jpg';
-var img4 = 'https://i.postimg.cc/j5T9x8L4/f0304022-da50-4a0a-8be4-6b4138bab3af.jpg'; 
-var img5 = 'https://i.postimg.cc/j5T9x8L4/f0304022-da50-4a0a-8be4-6b4138bab3af.jpg';
-var img6 = 'https://i.postimg.cc/8zq93kG3/c9737e9e-4e19-4f75-95d3-e56c746fb97e.jpg';
-var img7 = 'https://i.postimg.cc/8zq93kG3/c9737e9e-4e19-4f75-95d3-e56c746fb97e.jpg';
-var img8 = 'https://i.postimg.cc/GtK1wWKD/db93517c-0588-4a1b-b9a7-641b235597ad.jpg';
-var img9 = 'https://i.postimg.cc/cLb0JNSM/474106d3-382b-4964-af1e-fbfb35d495cd.jpg';
+var img = 'https://i.postimg.cc/Y9t2mzvt/Menu2.jpg'
+var img1 = 'https://i.postimg.cc/T386DJsB/Menu3.jpg'
+var img2 = 'https://i.postimg.cc/T386DJsB/Menu3.jpg'
+var img3 = 'https://i.postimg.cc/43sR8Ns5/Menu4.jpg'
+var img4 = 'https://i.postimg.cc/j5T9x8L4/f0304022-da50-4a0a-8be4-6b4138bab3af.jpg'
+var img5 = 'https://i.postimg.cc/j5T9x8L4/f0304022-da50-4a0a-8be4-6b4138bab3af.jpg'
+var img6 = 'https://i.postimg.cc/8zq93kG3/c9737e9e-4e19-4f75-95d3-e56c746fb97e.jpg'
+var img7 = 'https://i.postimg.cc/8zq93kG3/c9737e9e-4e19-4f75-95d3-e56c746fb97e.jpg'
+var img8 = 'https://i.postimg.cc/GtK1wWKD/db93517c-0588-4a1b-b9a7-641b235597ad.jpg'
+var img9 = 'https://i.postimg.cc/cLb0JNSM/474106d3-382b-4964-af1e-fbfb35d495cd.jpg'
 var img10 = 'https://i.postimg.cc/PqvxZdBD/06efe094-8f28-444e-b3d4-7474a9ee7f3e.jpg'
 
-var iimg1 = 'https://i.postimg.cc/t4KK7yy7/11fe6d78ece25ee972ee8de229e182a9.jpg';
-var iimg2 = 'https://i.postimg.cc/5t5R0Cnk/66945863cdf7c56b212c2280e91d497b.jpg';
-var iimg3 = 'https://i.postimg.cc/x1D5zdJr/0c967c4af27aa805391e3be495936acd.jpg';
-var iimg4 = 'https://i.postimg.cc/kM2LNB4D/89aaf3a76c5af6c6904716889463a278.jpg';
+var iimg1 = 'https://i.postimg.cc/t4KK7yy7/11fe6d78ece25ee972ee8de229e182a9.jpg'
+var iimg2 = 'https://i.postimg.cc/5t5R0Cnk/66945863cdf7c56b212c2280e91d497b.jpg'
+var iimg3 = 'https://i.postimg.cc/x1D5zdJr/0c967c4af27aa805391e3be495936acd.jpg'
+var iimg4 = 'https://i.postimg.cc/kM2LNB4D/89aaf3a76c5af6c6904716889463a278.jpg'
 var iimg5 = 'https://i.postimg.cc/gJntKTG1/e872a51954db06122d6108df482ef157.jpg'
 
 
@@ -48,17 +48,17 @@ global.naufraRedes = [yt, ig, md, paypal, git, linkwabot, email, grupo1, grupo2,
 global.naufraImg = [img, img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, iimg1, iimg2, iimg3, iimg4, iimg5, imagen1, imagen2, imagen3]
 global.naufraAll = [grupo1, ngrupo2, canal, yt, ig, md, paypal, git, linkwabot, email, grupo1, grupo2, canal, paypal2]
 
-global.yt = 'https://youtube.com/@user-bw5wl4ye8r?si=qN8bP4ZdKm3yXwbx';
-global.ig = 'https://www.instagram.com/naufrazapp?igsh=Y2g1ZXJxbm53eXAy';
-global.md = 'https://github.com/MultiBot-OFC/NaufraZapp-MD';
-global.paypal = 'https://www.paypal.me/NaufragoSolano';
-global.git = 'https://github.com/MultiBot-OFC/NaufraZapp-MD';
-global.linkwabot = 'https://wa.me/5493873687620';
-global.email = 'guardianbotmdoficial@gmail.com';
-global.grupo1 = 'https://chat.whatsapp.com/I7A2Q6EXtmE6d7zOxyYF3L';
-global.grupo2 = 'https://chat.whatsapp.com/I7A2Q6EXtmE6d7zOxyYF3L'; 
-global.canal = 'https://whatsapp.com/channel/0029VaRibRvDuMRj6ozMSN1l';
-global.paypal2 = 'https://www.paypal.me/ColaboracionBotOFC';
+global.yt = 'https://youtube.com/@user-bw5wl4ye8r?si=qN8bP4ZdKm3yXwbx'
+global.ig = 'https://www.instagram.com/naufrazapp?igsh=Y2g1ZXJxbm53eXAy'
+global.md = 'https://github.com/MultiBot-OFC/NaufraZapp-MD'
+global.paypal = 'https://www.paypal.me/NaufragoSolano'
+global.git = 'https://github.com/MultiBot-OFC/NaufraZapp-MD'
+global.linkwabot = 'https://wa.me/5493873687620'
+global.email = 'guardianbotmdoficial@gmail.com'
+global.grupo1 = 'https://chat.whatsapp.com/I7A2Q6EXtmE6d7zOxyYF3L'
+global.grupo2 = 'https://chat.whatsapp.com/I7A2Q6EXtmE6d7zOxyYF3L'
+global.canal = 'https://whatsapp.com/channel/0029VaRibRvDuMRj6ozMSN1l'
+global.paypal2 = 'https://www.paypal.me/ColaboracionBotOFC'
 
 global.nzVideos = ['https://qu.ax/ygwT.mp4', 'https://qu.ax/iFCi.mp4', 'https://qu.ax/jie.mp4', 'https://qu.ax/Pbha.mp4', 'https://qu.ax/bdvm.mp4']
 global.nzAll = ['https://qu.ax/ygwT.mp4', 'https://qu.ax/iFCi.mp4', 'https://qu.ax/jie.mp4', 'https://qu.ax/Pbha.mp4', 'https://qu.ax/bdvm.mp4', iimg1, iimg2, iimg3, iimg4, iimg5]
