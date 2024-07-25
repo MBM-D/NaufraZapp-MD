713c40c4185d8f572dc47f769fed31ac0c85bbfc
𖥔 ࣪˖🍁「𝗠𝘂𝗹𝘁𝗶𝗕𝗼𝘁┃ᵒᶠᶜ-ᵃᶜᵒᵘⁿᵗ」🍁𖥔 ࣪˖ <alanluliovargas@gmail.com>
2024-07-25 03:51:38 -0300

    • ⏣ 𝙉𝘢𝘶𝘧𝘳𝘢𝙕𝘢𝘱𝘱-𝙈𝘋 ⏣ •

--- store.js
+++ store.js
@@ -44,7 +44,7 @@ var iimg5 = 'https://i.postimg.cc/gJntKTG1/e872a51954db06122d6108df482ef157.jpg'
 global.ImgAll = [img, img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, imagen1, imagen2, imagen3]
 global.nzImg = [iimg1, iimg2, iimg3, iimg4, iimg5]
 //global.linkNz = [ngrupo1, ngrupo2, canal]
-global.naufraRedes = [yt, ig, md, paypal, git, linkwabot, email, grupo1, grupo2, canal, paypal2]
+//global.naufraRedes = [yt, ig, md, paypal, git, linkwabot, email, grupo1, grupo2, canal, paypal2]
 global.naufraImg = [img, img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, iimg1, iimg2, iimg3, iimg4, iimg5, imagen1, imagen2, imagen3]
 global.naufraAll = [grupo1, ngrupo2, canal, yt, ig, md, paypal, git, linkwabot, email, grupo1, grupo2, canal, paypal2]
 
