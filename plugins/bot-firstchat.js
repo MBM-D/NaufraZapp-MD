fce6c4c4f06fa60d808c843eaf017b88741853e2
𖥔 ࣪˖🍁「𝗠𝘂𝗹𝘁𝗶𝗕𝗼𝘁┃ᵒᶠᶜ-ᵃᶜᵒᵘⁿᵗ」🍁𖥔 ࣪˖ <alanluliovargas@gmail.com>
2024-07-29 01:55:27 -0300

    • ⏣ 𝙉𝘢𝘶𝘧𝘳𝘢𝙕𝘢𝘱𝘱-𝙈𝘋 ⏣ •

--- plugins/bot-firstchat.js
+++ plugins/bot-firstchat.js
@@ -8,27 +8,10 @@ let user = global.db.data.users[m.sender]
 
 if (new Date() - user.pc < 86400000) return
 await conn.reply(m.chat, `Hola usuario/a, gracias por estar aqui
-bienvenido/a a *NaufraZapp-Beta*
+bienvenido/a a *NaufraZapp-MD*
 
-❤️ Tu nombre:
-• ${nombre}
-
-👀 ¿Quieres ser un sub bot?
-puedes usar el comando:
-
-Por codigo de 8 dígitos:
-*#serbot --code*
-
-Por código QR:
-*#serbot*
-
-Si quieres contactar con soporte, o con el creador, usa estos comandos:
-
-Creador:
-*#naufrago*
-
-Soporte NaufraZapp-Beta:
-*#soporte*
+- Para ver el menu, use el comando:
+*#menu*
 `, m, fpay, )
 
 
