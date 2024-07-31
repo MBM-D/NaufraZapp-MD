bc6f40345550b5c4cea1f4842da6d2e6a0466c3c
NaufraZapp <161604719+NaufraZapp@users.noreply.github.com>
2024-07-31 14:38:00 -0500

    Update creador-update2.js

--- plugins/creador-update2.js
+++ plugins/creador-update2.js
@@ -1,37 +1,49 @@
-import { execSync } from 'child_process';
+import { execSync } from 'child_process'
 
-const handler = async (m, { conn, text }) => {
+var handler = async (m, { conn, text }) => {
+
+
 try {
+
 const stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''));
 let messager = stdout.toString()
-if (messager.includes('Already up to date.')) messager = `❮✅❯ » 𝙉𝙖𝙪𝙛𝙧𝙖𝙕𝙖𝙥𝙥-𝙈𝘿 𝙮𝙖 𝙚𝙨𝙩𝙖 𝙚𝙣 𝙪𝙣𝙖 𝙫𝙚𝙧𝙨𝙞𝙤𝙣 𝙖𝙘𝙩𝙪𝙖𝙡𝙞𝙯𝙖𝙙𝙖.`
-if (messager.includes('Updating')) messager = `❮✅❯ » 𝘼𝘾𝙏𝙐𝘼𝙇𝙄𝙕𝘼𝘿𝙊: ` + stdout.toString()
-conn.reply(m.chat, messager, m);
-} catch {      
-try {    
-const status = execSync('git status --porcelain');
+
+if (messager.includes('✅ 𝙉𝙖𝙪𝙛𝙧𝙖𝙕𝙖𝙥𝙥-𝙈𝘿 𝙮𝙖 𝙨𝙚 𝙚𝙣𝙘𝙪𝙚𝙣𝙩𝙧𝙖 𝙖𝙘𝙩𝙪𝙖𝙡𝙞𝙯𝙖𝙙𝙤.')) messager = '✅ 𝙉𝙖𝙪𝙛𝙧𝙖𝙕𝙖𝙥𝙥-𝙈𝘿 𝙮𝙖 𝙚𝙨𝙩𝙖 𝙖 𝙡𝙖 𝙫𝙚𝙧𝙨𝙞𝙤𝙣 𝙢𝙖𝙨 𝙧𝙚𝙘𝙞𝙚𝙣𝙩𝙚.'
+
+if (messager.includes('• 𝘼𝙘𝙩𝙪𝙖𝙡𝙞𝙯𝙖𝙣𝙙𝙤...')) messager = '• 𝘾𝙖𝙧𝙜𝙖𝙣𝙙𝙤 𝙙𝙖𝙩𝙤𝙨 𝙖𝙘𝙩𝙪𝙖𝙡𝙞𝙯𝙖𝙙𝙤𝙨, 𝙚𝙨𝙥𝙚𝙧𝙚 𝙪𝙣 𝙢𝙤𝙢𝙚𝙣𝙩𝙤...\n\n' + stdout.toString()
+conn.reply(m.chat, messager, m, canal,)
+
+} catch { 
+try {
+
+const status = execSync('git status --porcelain')
+
 if (status.length > 0) {
-const conflictedFiles = status
-.toString()
-.split('\n')
-.filter(line => line.trim() !== '')
-.map(line => {
-if (line.includes('.npm/') || line.includes('.cache/') || line.includes('tmp/') || line.includes('ExoticoBotSession/') || line.includes('npm-debug.log')) {
-return null;
+const conflictedFiles = status.toString().split('\n').filter(line => line.trim() !== '').map(line => {
+if (line.includes('.npm/') || line.includes('.cache/') || line.includes('tmp/') || line.includes('NaufraBotSession/') || line.includes('npm-debug.log')) {
+return null
 }
-return '*➤ ' + line.slice(3) + '*'})
-.filter(Boolean);
+return '*→ ' + line.slice(3) + '*'}).filter(Boolean)
 if (conflictedFiles.length > 0) {
-const errorMessage = `❮✅❯ » 𝘼𝙘𝙩𝙪𝙖𝙡𝙞𝙯𝙖𝙙𝙤 𝙘𝙤𝙣 𝙚𝙭𝙞𝙩𝙤:\n\nArchivo: ${conflictedFiles.join('\n')}.*`
-await conn.reply(m.chat, errorMessage, m);  
-}}
-} catch (error) {
-console.error(error);
-if (error.message) {
-errorMessage2 += `\n❮❌❯ » 𝙀𝙧𝙧𝙤𝙧: ` + error.message;
+const errorMessage = `❌ Se han hecho cambios locales qué entran en conflicto con las Actualizaciones del Repositorio, Para actualizar, reinstala el Bot o realiza las actualizaciones manualmente.\n\n*ARCHIVOS EN CONFLICTO:*\n\n${conflictedFiles.join('\n')}`
+await conn.reply(m.chat, errorMessage, m, canal,)
 }
-await m.reply(`❮⚠️❯ » 𝙊𝙘𝙪𝙧𝙧𝙞𝙤 𝙪𝙣 𝙚𝙧𝙧𝙤𝙧, 𝙞𝙣𝙩𝙚𝙣𝙩𝙖𝙡𝙤 𝙙𝙚 𝙣𝙪𝙚𝙫𝙤.`) 
-}}};
-handler.command = /^(reinstalar|updat|gitpull)$/i;
-handler.rowner = true;
-export default handler;
+}
+} catch (error) {
+console.error(error)
+let errorMessage2 = '❌ 𝙊𝙘𝙪𝙧𝙧𝙞𝙤 𝙪𝙣 𝙚𝙧𝙧𝙤𝙧 𝙞𝙣𝙚𝙨𝙥𝙚𝙧𝙖𝙙𝙤.'
+if (error.message) {
+errorMessage2 += '\n𝙀𝙍𝙍𝙊𝙍: ' + error.message;
+}
+await conn.reply(m.chat, errorMessage2, m, canal,)
+}
+}
+
+}
+
+handler.help = ['update', 'actualizar']
+handler.tags = ['owner']
+handler.command = ['update', 'actualizar']
+handler.rowner = true
+
+export default handler
