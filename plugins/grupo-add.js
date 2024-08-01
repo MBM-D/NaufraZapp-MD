419f949db2b30279dd9c83e3a16206b0400d7889
𖥔 ࣪˖🍁「𝗠𝘂𝗹𝘁𝗶𝗕𝗼𝘁┃ᵒᶠᶜ-ᵃᶜᵒᵘⁿᵗ」🍁𖥔 ࣪˖ <alanluliovargas@gmail.com>
2024-08-01 05:14:27 -0300

    • ⏣ 𝙉𝘢𝘶𝘧𝘳𝘢𝙕𝘢𝘱𝘱-𝙈𝘋 ⏣ •

--- plugins/grupo-add.js
+++ plugins/grupo-add.js
@@ -1,28 +1,22 @@
-var handler = async (m, { conn, args, text, usedPrefix, command }) => {
-
+let handler = async (m, { conn, args, text, usedPrefix, command }) => {
 let who 
 if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
 else who = m.chat
 let name = await conn.getName(m.sender)	
 let user = global.db.data.users[who]
-let nom = conn.getName(m.sender)
-if (!global.db.data.settings[conn.user.jid].restrict) return conn.reply(m.chat, `『✦』Esta funcion no es compatible para *Sub-Bots*`, m)
-if (!text) return conn.reply(m.chat, `*『✦』Ingrese el numero de la persona sin el (+), por ejemplo:\n\n#${command} 5217299999999`, m)
-if (text.includes('+')) return conn.reply(m.chat, `『✦』Error, debe de ingresar el numero sin el código (+).`, m )
+if (!global.db.data.settings[conn.user.jid].restrict) return conn.reply(m.chat, `Las restricciones no estan activas en el bot.`, m) 
+if (!text) throw `Ingrese un numero valido para agregarlo, por ejemplo:\n*${usedPrefix + command}* 59355555555`
+if (text.includes('+')) throw  `Ingrese un numero valido para agregarlo, por ejemplo:\n*${usedPrefix + command}* 59355555555`
 let group = m.chat
 let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
- 
-await conn.reply(text+'@s.whatsapp.net', `*『✦』Hola soy un bot de WhatsApp, alguien te invito a un grupo.*\n\n*『✦』LINK/ENLACE:*\n${link}\n\n『😃』Te estamos esperando.`, m, {mentions: [m.sender]} )
-conn.reply(m.chat, `*『✅』La invitación se ha enviado al privado de:*\n${nom}`, m) 
-
+await conn.reply(text+'@s.whatsapp.net', `Holaaa, soy NaufraZapp-MD, un bot de WhatsApp, alguien te invito a unirte al grupo, te estamos esperando!!!!\n\n${link}`, m, {mentions: [m.sender]})
+m.reply(`✅ Link enviado a *@${who.split`@`[0]}*`) 
 }
-handler.help = ['add']
-handler.tags = ['grupo']
+handler.help = ['add', '+'].map(v => v + ' número')
+handler.tags = ['group']
 handler.command = /^(add|agregar|invitar|invite|añadir|\+)$/i
-
 handler.group = true
 handler.admin = true
 handler.botAdmin = true
 handler.fail = null
-
 export default handler
