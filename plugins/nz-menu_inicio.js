2793f1a3ff632500a765a536f3590944e327cd80
NaufraZapp <161604719+NaufraZapp@users.noreply.github.com>
2024-08-01 16:57:23 -0500

    Update nz-menu_inicio.js

--- plugins/nz-menu_inicio.js
+++ plugins/nz-menu_inicio.js
@@ -18,7 +18,7 @@ Antes de comenzar a utilizar el bot, no se olvide de *verificarse o registrarse*
 await conn.sendButton(m.chat, menu, wm, imagebot, [
 ['𝙈𝙀𝙉𝙐 𝘾𝙊𝙈𝙋𝙇𝙀𝙏𝙊 💥', '.menucompleto'],
 ['𝘼𝙐𝙏𝙊 𝙑𝙀𝙍𝙄𝙁𝙄𝘾𝘼𝙍 ✅', '#reg nauSub.20']], null, [
-['𝘾𝘼𝙉𝘼𝙇 𝙊𝙁𝙄𝘾𝙄𝘼𝙇🌎', `${channel}`]], null, null, fkontak)}
+['𝘾𝘼𝙉𝘼𝙇 𝙊𝙁𝙄𝘾𝙄𝘼𝙇🌎', `${canal}`]], null, null, fkontak)}
 handler.command = /^menu|help|comandos|memu|menú|memú$/i
 handler.register = true
 export default handler
