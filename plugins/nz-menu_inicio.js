edb95d6ce84f8cc98f913f087dcf8c82fcd8aea9
NaufraZapp <161604719+NaufraZapp@users.noreply.github.com>
2024-08-01 16:55:09 -0500

    Update nz-menu_inicio.js

--- plugins/nz-menu_inicio.js
+++ plugins/nz-menu_inicio.js
@@ -15,7 +15,7 @@ También pueden seguirnos en nuestro canal oficial de WhatsApp para recibir info
 
 Antes de comenzar a utilizar el bot, no se olvide de *verificarse o registrarse* para usar todos los comandos libremente👇
 `
-await conn.sendButton(m.chat, menu, wm, pp, [
+await conn.sendButton(m.chat, menu, wm, imagebot, [
 ['𝙈𝙀𝙉𝙐 𝘾𝙊𝙈𝙋𝙇𝙀𝙏𝙊 💥', '.menucompleto'],
 ['𝘼𝙐𝙏𝙊 𝙑𝙀𝙍𝙄𝙁𝙄𝘾𝘼𝙍 ✅', '#reg nauSub.20']], null, [
 ['𝘾𝘼𝙉𝘼𝙇 𝙊𝙁𝙄𝘾𝙄𝘼𝙇🌎', `${channel}`]], null, null, fkontak)}
