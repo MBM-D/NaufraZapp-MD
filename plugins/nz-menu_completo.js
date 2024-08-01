7604636d0307fd73d316a5c2f48929cff4badb05
𖥔 ࣪˖🍁「𝗠𝘂𝗹𝘁𝗶𝗕𝗼𝘁┃ᵒᶠᶜ-ᵃᶜᵒᵘⁿᵗ」🍁𖥔 ࣪˖ <alanluliovargas@gmail.com>
2024-08-01 04:40:42 -0300

    • ⏣ 𝙉𝘢𝘶𝘧𝘳𝘢𝙕𝘢𝘱𝘱-𝙈𝘋 ⏣ •

--- plugins/nz-menu_completo.js
+++ plugins/nz-menu_completo.js
@@ -1,9 +1,9 @@
-let imagebot = './Menu2.jpg'
+let imagebot = './Menu1.jpg'
 let handler = async (m, { conn, command }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let str = `
 ╭─────────────────···•⊰ ⋆
-│👋🏻 Hola ${taguser}
+│👋🏻 Hola usuario/a
 │soy NaufraZapp-MD
 ╰─────────────────···•⊰ ⋆
 
