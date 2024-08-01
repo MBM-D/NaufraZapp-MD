fbbd306ecb87b9dc384d62a019044e228803d876
𖥔 ࣪˖🍁「𝗠𝘂𝗹𝘁𝗶𝗕𝗼𝘁┃ᵒᶠᶜ-ᵃᶜᵒᵘⁿᵗ」🍁𖥔 ࣪˖ <alanluliovargas@gmail.com>
2024-08-01 03:00:13 -0300

    Delete plugins directory

--- plugins/creador-banchat.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m) => {
-global.db.data.chats[m.chat].isBanned = true
-m.reply(`❮✅❯ » 𝙉𝙖𝙪𝙛𝙧𝙖𝙕𝙖𝙥𝙥-𝙈𝘿 𝙣𝙤 𝙧𝙚𝙨𝙥𝙤𝙣𝙙𝙚𝙧𝙖 𝙣𝙞𝙣𝙜𝙪𝙣 𝙘𝙤𝙢𝙖𝙣𝙙𝙤 𝙚𝙣 𝙚𝙨𝙩𝙚 𝙜𝙧𝙪𝙥𝙤.`)
-}
-handler.help = ['banchat']
-handler.tags = ['owner']
-handler.command = /^banchat|banexotic|banchat2$/i
-handler.rowner = true
-export default handler
