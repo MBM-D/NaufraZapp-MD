93539a5a8f9f5b1f86aecae37f69ff302c3e4ca5
NaufraZapp <161604719+NaufraZapp@users.noreply.github.com>
2024-08-01 16:44:18 -0500

    Update nz-menu_inicio.js

--- plugins/nz-menu_inicio.js
+++ plugins/nz-menu_inicio.js
@@ -3,11 +3,22 @@ let handler = async (m, { conn, command }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let str = `•═══• ❮🌎 \`BIENVENIDO/A\` 🌎❯ •═══•
 
-- *_Para ver el menu completo, presione el boton de_*
-• \`Ir al menu\``
-await conn.sendButton(m.chat, str, wm, imagebot, [
-['IR AL MENU 🪐', '.allmenu'],
-['GRUPOS 🔜', '#gruposnz']], null, null, fkontak)}
+☞ 👑𝙉𝙖𝙪𝙛𝙧𝙖𝙕𝙖𝙥𝙥-𝙈𝘿👑
+
+☛  NɑufrɑZɑpp-MD es un creɑdo pɑrɑ *fɑcilitɑr el mɑnejo de tus grupos de Whɑtsɑpp* y *brindɑrte errɑmientɑs de entretenimiento pɑrɑ tus grupos.*  
+
+💥ɑnteriormente este bot erɑ *privɑdo* pero devido ɑ que muchos usuɑrios tɑmbién queriɑn instɑlɑrlo por ellos mismos se lɑnzo lɑ versión públicɑ de este mɑrɑvilloso bot, pɑrɑ que todos puedɑn disfrutɑrlo💥
+
+🫂ɢʀᴀᴄɪᴀs ᴀ ᴛᴏᴅᴏs ʟᴏs ᴄᴏʟᴀʙᴏʀᴀᴅᴏʀᴇs ᴘᴏʀ ᴇsᴛᴇ ᴍᴀʀᴀᴠɪʟʟᴏsᴏ ᴘʀᴏʏᴇᴄᴛᴏ🤗
+
+También pueden seguirnos en nuestro canal oficial de WhatsApp para recibir información del bot y también para participar en algunos sorteos💥
+
+Antes de comenzar a utilizar el bot, no se olvide de *verificarse o registrarse* para usar todos los comandos libremente👇
+`
+await conn.sendButton(m.chat, menu, wm, pp, [
+['𝙈𝙀𝙉𝙐 𝘾𝙊𝙈𝙋𝙇𝙀𝙏𝙊 💥', '.menucompleto'],
+['𝘼𝙐𝙏𝙊 𝙑𝙀𝙍𝙄𝙁𝙄𝘾𝘼𝙍 ✅', '#reg nauSub.20']], null, [
+['𝘾𝘼𝙉𝘼𝙇 𝙊𝙁𝙄𝘾𝙄𝘼𝙇🌎', `${channel}`]], null, null, fkontak)}
 handler.command = /^menu|help|comandos|memu|menú|memú$/i
 handler.register = true
 export default handler
