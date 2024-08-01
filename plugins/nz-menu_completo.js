e6ef561d8a4f1c6e83ebd8409159aaae962b3cdf
𖥔 ࣪˖🍁「𝗠𝘂𝗹𝘁𝗶𝗕𝗼𝘁┃ᵒᶠᶜ-ᵃᶜᵒᵘⁿᵗ」🍁𖥔 ࣪˖ <alanluliovargas@gmail.com>
2024-08-01 04:40:07 -0300

    • ⏣ 𝙉𝘢𝘶𝘧𝘳𝘢𝙕𝘢𝘱𝘱-𝙈𝘋 ⏣ •

--- plugins/nz-menu_completo.js
+++ plugins/nz-menu_completo.js
@@ -1,4 +1,204 @@
-import fetch from 'node-fetch';
+let imagebot = './Menu2.jpg'
+let handler = async (m, { conn, command }) => {
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+let str = `
+╭─────────────────···•⊰ ⋆
+│👋🏻 Hola ${taguser}
+│soy NaufraZapp-MD
+╰─────────────────···•⊰ ⋆
+
+╭• •꒰─•⭐·𝙀𝘾𝙊𝙉𝙊𝙈𝙄𝘼 : 𝙍𝙋𝙂۬⭐ٜ･
+│•┐ _${usedPrefix}cofre_
+┃•│ _${usedPrefix}levelup_
+┃•│ _${usedPrefix}robar_
+┃•│ _${usedPrefix}unreg_
+┃•│ _${usedPrefix}reg_
+┃•│ _${usedPrefix}minar_
+┃•│ _${usedPrefix}minar2_
+┃•│ _${usedPrefix}minar3_
+┃•┘ _${usedPrefix}work_
+╰• •───• •───• •───•
+
+┍━━━━━━━━━━━━━━━━━━━•𖥔 ࣪˖
+│•꒰─•🗒️·𝙄𝙉𝙁𝙊𝙍𝙈𝘼𝘾𝙄𝙊𝙉ٜ۬🗒️･
+┕━━━━━━━━━━━━━━━━━━━•𖥔 ࣪˖
+┌• ${usedPrefix}estado
+└•⌕ *ver estado del bot*
+┌• ${usedPrefix}nzgrupos
+└•⌕ *ver grupos oficiales*
+┌• ${usedPrefix}tipobot
+└•⌕ *información del bot*
+┌• ${usedPrefix}on / off
+└•⌕ *muestra las funciones.*
+┌• ${usedPrefix}perfil
+└•⌕ *información de tu perfil*
+
+┍━━━━━━━━━━━━━━━━━━━•𖥔 ࣪˖
+│•꒰─•🤖·𝙎𝙀𝙍𝘽𝙊𝙏 : 𝙉𝙕ٜ۬🤖･
+┕━━━━━━━━━━━━━━━━━━━•𖥔 ࣪˖
+┌• ${usedPrefix}serbot
+└•⌕ *Registra con código de 8 dígitos*
+┌• ${usedPrefix}borrarsesion_
+└•⌕ *borra todos los sub bots*
+┌• ${usedPrefix}stop
+└•⌕ *apagar mi sub bot*
+┌• ${usedPrefix}bcbots
+└•⌕ *mensaje para todos los subbots*
+┌• ${usedPrefix}ds_
+└•⌕ *Registra con código de 8 dígitos*
+┌• ${usedPrefix}bots
+└•⌕ *ver sub bots*
+╰• •───• •───• •───•
+
+┍━━━━━━━━━━━━━━━━━━━•𖥔 ࣪˖
+│.      •꒰─•🎰·𝙅𝙐𝙀𝙂𝙊𝙎ٜ۬🎰･
+┕━━━━━━━━━━━━━━━━━━━•𖥔 ࣪˖
+┌• ${usedPrefix}acertijo
+└•⌕ *Acertijo.*
+┌• ${usedPrefix}tictactoe
+└•⌕ *Tictactoe*
+┌• ${usedPrefix}delttt
+└•⌕ *delttt*
+
+┍━━━━━━━━━━━━━━━━━━━•𖥔 ࣪˖
+│.      •꒰─•🌎·𝙈𝙀𝙉𝙐𝙎ٜ۬🌎･
+┕━━━━━━━━━━━━━━━━━━━•𖥔 ࣪˖
+┌• ${usedPrefix}menu
+└•⌕ *Menu principal*
+┌• ${usedPrefix}menu18
+└•⌕ *Menu para adultos*
+┌• ${usedPrefix}menuaudios
+└•⌕ *menu audios*
+
+┍━━━━━━━━━━━━━━━━━━━•𖥔 ࣪˖
+│•꒰─•🧰·𝙃𝙀𝙍𝙍𝘼𝙈𝙄𝙀𝙉𝙏𝘼𝙎ٜ۬🧰･
+┕━━━━━━━━━━━━━━━━━━━•𖥔 ࣪˖
+┌• ${usedPrefix}chatgpt
+└•⌕ *texto*
+┌• ${usedPrefix}chatgptvoz
+└•⌕ *texto*
+┌• ${usedPrefix}bot
+└•⌕ *texto*
+┌• ${usedPrefix}delchatgpt
+└•⌕ *elimina sesión gpt*
+┌• ${usedPrefix}hd
+└•⌕ *imagen*
+┌• ${usedPrefix}traducir
+└•⌕ *mensaje traducido*
+┌• ${usedPrefix}toimg
+└•⌕ *Responde a sticker.*
+┌• ${usedPrefix}tourl
+└•⌕ *Guardar sticker.*
+┌• ${usedPrefix}sticker
+└•⌕ *Imagen o video*
+
+┍━━━━━━━━━━━━━━━━━━━•𖥔 ࣪˖
+│「⬇️ִֶָ 𖥔 ࣪˖𝑫𝒆𝒔𝒄𝒂𝒓𝒈𝒂𝒔ִֶָ 𖥔 ࣪˖⬇️」
+┕━━━━━━━━━━━━━━━━━━━•𖥔 ࣪˖
+┌• ${usedPrefix}fb
+└•⌕ *enlace*
+┌• ${usedPrefix}apk
+└•⌕ *nombre*
+┌• ${usedPrefix}mediafire
+└•⌕ *enlace*
+┌• ${usedPrefix}ig
+└•⌕ *enlace*
+┌• ${usedPrefix}tiktok
+└•⌕ *enlace*
+┌• ${usedPrefix}twitter
+└•⌕ *enlace*
+┌• ${usedPrefix}pinterest
+└•⌕ *texto*
+┌• ${usedPrefix}google
+└•⌕ *texto*
+┌• ${usedPrefix}mega
+└•⌕ *enlace*
+┌• ${usedPrefix}gitclone
+└•⌕ *enlace*
+┌• ${usedPrefix}playdoc2
+└•⌕ *enlace • texto*
+┌• ${usedPrefix}playdoc
+└•⌕ *enlace • texto*
+┌• ${usedPrefix}quemusica
+└•⌕ *enlace • texto*
+┌• ${usedPrefix}play
+└•⌕ *enlace • texto*
+┌• ${usedPrefix}play2
+└•⌕ *enlace • texto*
+┌• ${usedPrefix}dlav
+└•⌕ *enlace • texto*
+
+┍━━━━━━━━━━━━━━━━━━━•𖥔 ࣪˖
+│•꒰─•👥·𝙋𝘼𝙍𝘼 𝙂𝙍𝙐𝙋𝙊𝙎ٜ۬👥･
+┕━━━━━━━━━━━━━━━━━━━•𖥔 ࣪˖
+┌• ${usedPrefix}admins
+└•⌕ *llamar a los admins*
+┌• ${usedPrefix}add
+└•⌕ *numero +54xxxxxxx*
+┌• ${usedPrefix}grupo
+└•⌕ *abrir : cerrar*
+┌• ${usedPrefix}demote
+└•⌕ *quitar admin*
+┌• ${usedPrefix}promote
+└•⌕ *dar admin*
+┌• ${usedPrefix}ban
+└•⌕ *@tag*
+┌• ${usedPrefix}link
+└•⌕ *mostrar enlace*
+┌• ${usedPrefix}resetlink
+└•⌕ *cambiar link grupal*
+┌• ${usedPrefix}tagall
+└•⌕ *mensionar a todos*
+┌• ${usedPrefix}fantasmas
+└•⌕ *ver inactivos*
+┌• ${usedPrefix}kickfantasmas
+└•⌕ *eliminar inactivos*
+┌• ${usedPrefix}hidetag
+└•⌕ *mensionar con mensaje*
+┌• ${usedPrefix}setwelcome
+└•⌕ *texto*
+┌• ${usedPrefix}setbye
+└•⌕ *texto*
+┌• ${usedPrefix}setdesc
+└•⌕ *texto*
+┌• ${usedPrefix}setfoto
+└•⌕ *img*
+┌• ${usedPrefix}setname
+└•⌕ *texto*
+┌• ${usedPrefix}grupoid
+└•⌕ *id del grupo*
+┌• ${usedPrefix}infogrupo
+└•⌕ *información del grupo*
+╰• •───• •───• •───•
+
+┍━━━━━━━━━━━━━━━━━━━•𖥔 ࣪˖
+│•꒰─•👑·𝙋𝙍𝙊𝙋𝙄𝙀𝙏𝘼𝙍𝙄𝙊۬👑ٜ･
+┕━━━━━━━━━━━━━━━━━━━•𖥔 ࣪˖
+┌• ${usedPrefix}backup
+└•⌕ *backup dol bot.*
+┌• ${usedPrefix}update
+└•⌕ *Actualizar bot.*
+┌• ${usedPrefix}reporte
+└•⌕ *reportar comando*
+┌• ${usedPrefix}unbanuser
+└•⌕ *Desbanear el usuario.*
+┌• ${usedPrefix}banuser
+└•⌕ *Banear el usuario.*
+┌• ${usedPrefix}banchat
+└•⌕ *Banear el chat.*
+┌• ${usedPrefix}unbanchat
+└•⌕ *Desbanear el chat.*
+`
+await conn.sendButton(m.chat, str, wm, imagebot, [
+['𝗜𝗡𝗦𝗧𝗔𝗟𝗔𝗥 𝗕𝗢𝗧 🤖', '.installbot'],
+['𝗘𝗦𝗧𝗔𝗗𝗢 🪐', '#estado'],
+['𝗚𝗥𝗨𝗣𝗢𝗦 👥', '/gruposnz'], null, null, fkontak)}
+handler.command = /^allmenu|menucompleto|menuall|menutodo$/i
+//handler.private = true
+handler.register = true
+export default handler
+
+/*import fetch from 'node-fetch';
 const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
   if (usedPrefix == 'a' || usedPrefix == 'A') return;
   try {
@@ -249,3 +449,4 @@ function clockString(ms) {
   const s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60;
   return [h, m, s].map((v) => v.toString().padStart(2, 0)).join(':');
     }
+*/
