0fdc5a20be2623718c6671ea6dc14dccfb64dad2
Ramón <165967820+RamonFTGD@users.noreply.github.com>
2024-08-01 15:13:11 -0600

    Update nz-menu_completo.js

--- plugins/nz-menu_completo.js
+++ plugins/nz-menu_completo.js
@@ -1,4 +1,4 @@
-let imagebot = './Menu1.jpg'
+let imagebot = 'https://telegra.ph/file/4c4de4fbd4b87fc6c397d.jpg';
 let handler = async (m, { conn, usedPrefix, command }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let str = `
