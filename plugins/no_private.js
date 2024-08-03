ab6c5ae01f8e905b1caa93d9cf1359ba410e7fc6
Juan1986 <166744425+ByJuan1986@users.noreply.github.com>
2024-08-03 17:36:27 +0200

    Update no_private.js

--- plugins/no_private.js
+++ plugins/no_private.js
@@ -1,6 +1,23 @@
+let handler = m => m
+handler.all = async function (m) {
+let prefixRegex = new RegExp('^[' + (opts['prefix'] || '‎z/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.,\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
+
+let setting = global.db.data.settings[this.user.jid]
+const settingsREAD = global.db.data.settings[this.user.jid] || {}
+
+if (m.text && prefixRegex.test(m.text)) {
+await this.sendPresenceUpdate('composing', m.chat)
+await this.readMessages([m.key]) 
+        
+let usedPrefix = m.text.match(prefixRegex)[0]
+let command = m.text.slice(usedPrefix.length).trim().split(' ')[0]
+}} 
+
+export default handler  
+
 const comandos = /piedra|papel|tijera|estado|verificar|code|jadibot --code|--code|creadora|bottemporal|grupos|instalarbot|términos|bots|deletebot|eliminarsesion|serbot|verify|register|registrar|reg|reg1|nombre|name|nombre2|name2|edad|age|edad2|age2|genero|género|gender|identidad|pasatiempo|hobby|identify|finalizar|pas2|pas3|pas4|pas5|registroc|deletesesion|registror|jadibot/i
 
-let handler = m => m
+//let handler = m => m
 handler.before = async function (m, { conn, isOwner, isROwner }) {
 if (m.fromMe) return !0
 if (m.isGroup) return !1
@@ -14,9 +31,9 @@ user = global.db.data.users[m.sender]
 bot = global.db.data.settings[this.user.jid] || {}
 
 if (bot.antiPrivate && !isOwner && !isROwner) {
-return await conn.reply(m.chat, `❮⚠️❯ » \`𝙄𝙉𝙁𝙊𝙍𝙈𝘼𝘾𝙄𝙊𝙉 / 𝘼𝙑𝙄𝙎𝙊\`\n\n- _Hola usuario(a)_ @${m.sender.split`@`[0]}, _esta prohibido hablar privadamente con el bot._\n• _Puedes entrar al chat general en la comunidad para hablar ahi. ( no se permiten bots )\n • ${grupo1}\n\n- *_o tambien puedes entrar al grupo oficial del bot, para usar sus comandos._*\n• https://chat.whatsapp.com/EpQRslGep33GTHzd6LlRpm\n\n> Se te bloqueara el contacto en este chat.`, m, { mentions: [m.sender] })  
+return await conn.reply(m.chat, `❮⚠️❯ » \`𝙄𝙉𝙁𝙊𝙍𝙈𝘼𝘾𝙄𝙊𝙉 / 𝘼𝙑𝙄𝙎𝙊\`\n\n- _Hola usuario(a)_ @${m.sender.split`@`[0]}, _esta prohibido hablar privadamente con el bot._\n• _Puedes entrar al chat general en la comunidad para hablar ahi. ( no se permiten bots )\n • ${grupo1}\n\n- *_o tambien puedes entrar al grupo oficial del bot, para usar sus comandos._*\n• ${grupo2}\n\n> Se te bloqueara el contacto en este chat.`, m, { mentions: [m.sender] })  
 await this.updateBlockStatus(m.sender, 'block')
 }
 return !1
-} 
-export default handler
+}
+//export default handler
