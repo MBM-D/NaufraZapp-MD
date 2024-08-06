65961581753ed8037c66186f15f4bf39cc88d871
༺༂༆☄️Mangara☄️༆༂༻ <88118100+Atsushi321@users.noreply.github.com>
2024-08-06 14:24:40 -0500

    Update no_private.js

--- plugins/no_private.js
+++ plugins/no_private.js
@@ -1,8 +1,13 @@
-if (!m.isGroup && !isROwner) {  
-//const bot = global.db.data.users[m.sender] || {};
-if (global.db.data.settings[numBot].antiPrivate) {
-conn.sendMessage(m.chat, {text: `❮⚠️❯ » \`𝙄𝙉𝙁𝙊𝙍𝙈𝘼𝘾𝙄𝙊𝙉 / 𝘼𝙑𝙄𝙎𝙊\`\n\n- _Hola usuario(a)_ @${m.sender.split`@`[0]}, _esta prohibido hablar privadamente con el bot._\n• _Puedes entrar al chat general en la comunidad para hablar ahi. ( no se permiten bots )\n • ${grupo1}\n\n- *_o tambien puedes entrar al grupo oficial del bot, para usar sus comandos._*\n• ${grupo2}\n\n> Se te bloqueara el contacto en este chat.`, mentions: [m.sender], }, {quoted: m}) 
-await delay(2 * 2000) 
-await conn.updateBlockStatus(m.chat, 'block')   
-return 
-}}
+export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) {
+  if (m.isBaileys && m.fromMe) return !0;
+  if (m.isGroup) return !1;
+  if (!m.message) return !0;
+  if (m.text.includes('hola')) return !0;
+  const chat = global.db.data.chats[m.chat];
+  const bot = global.db.data.settings[this.user.jid] || {};
+  if (bot.antiPrivate && !isOwner && !isROwner) {
+    await m.reply(`❮⚠️❯ » \`𝙄𝙉𝙁𝙊𝙍𝙈𝘼𝘾𝙄𝙊𝙉 / 𝘼𝙑𝙄𝙎𝙊\`\n\n- _Hola usuario(a)_ @${m.sender.split`@`[0]}, _esta prohibido hablar privadamente con el bot._\n• _Puedes entrar al chat general en la comunidad para hablar ahi. ( no se permiten bots )\n • ${grupo1}\n\n- *_o tambien puedes entrar al grupo oficial del bot, para usar sus comandos._*\n• ${grupo2}\n\n> Se te bloqueara el contacto en este chat.`, false, {mentions: [m.sender]});
+    await this.updateBlockStatus(m.chat, 'block');
+  }
+  return !1;
+}
